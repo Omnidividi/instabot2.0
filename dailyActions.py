f053706490fefb90721b71c8ffb2c57af31b8d2d
Leonhard Soenke <ls_business@outlook.com>
2018-02-24 17:25:08 +0000

    =bug fixes

--- dailyActions.py
+++ dailyActions.py
@@ -4,7 +4,7 @@ from bot.config.config import config
 
 for account in accounts.configs:
 	print("{}: started at {}".format(account.name, now))
-	instaBot.instaBot(account).run()
+	instaBot.instaBot(account).dailyWrapUp()
 	print("{}: ended at {}".format(account.name, now))
 	for _ in range(3):
 		print("----------------------------------------------------")
